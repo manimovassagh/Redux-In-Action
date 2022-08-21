ef7d5977884a1d41516458e74cc00d2d2b1792c6
manimovassagh <mani.mgh@gmail.com>
2022-08-21 17:04:04 +0200

    add some docs

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from '../features/counter/counterSlice'
 
-
+// Some items For git showup
 function Home() {
     const count = useSelector((state) => state.counter.value)
   return (
